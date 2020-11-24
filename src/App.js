f2467ad8eb1956589648fb26141b0c2a08c7fe12
buckshot307 <buckshot307@gmail.com>
2020-11-23 23:04:50 -0500

    Got the api fetch working. Moving on to improving homepage look

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React from 'react';
 import Navbar from './components/navbar';
- 
-const App = ({ title }) =>
 
-  <div>
-    <Navbar />
-    {title}
-  </div>;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://aaron-key-blog-api.herokuapp.com/api")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <Navbar />
+          <ul>
+            {items.map(item => (
+              <li key={item._id}>
+                {item.title} {item.blogContent}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+}
 
 export default App;
