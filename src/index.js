14d654989fb5c920f625aa6888e7d4fca45639f7
buckshot307 <buckshot307@gmail.com>
2020-12-10 19:45:07 -0500

    Changed import to require()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './hooks/facebookScripts'
+require('./hooks/facebookScripts')
 require('./mystyles.scss')
 
 
