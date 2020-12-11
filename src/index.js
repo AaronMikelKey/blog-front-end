14fcfac74cf84bee2d783dd6368c4c1c1795075b
buckshot307 <buckshot307@gmail.com>
2020-12-10 19:40:35 -0500

    Added FB auth script
    Added FB auth script to index.js.  Added /hooks to /src.  Added useScript.js which takes a .js argument and injects as a JSX element since it might be needed later; not in use now.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-require('./mystyles.scss');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './hooks/facebookScripts'
+require('./mystyles.scss')
 
-const title = 'Aaron\'s Blog';
+
+const title = 'Aaron\'s Blog'
  
 ReactDOM.render(
     <App title={title} />,
   document.getElementById('app')
-);
+)
 
-module.hot.accept();
+module.hot.accept()
