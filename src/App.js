249073c993bca11cb5b124d391412aac1e8bfefa
buckshot307 <buckshot307@gmail.com>
2020-12-10 20:19:23 -0500

    moved it back to index.html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './Home'
 import About from './About'
 import Contact from './Contact'
 import BlogPost from './components/blogPost'
-import facebookScripts from './hooks/facebookScripts'
 import {
   BrowserRouter as Router,
   Switch,
@@ -25,7 +24,6 @@ const SingleBlogPost = () => {
 const App = () => {
   return (
     <Router>
-      <facebookScripts />
       <Switch>
         <Route path="/about">
           <About />
