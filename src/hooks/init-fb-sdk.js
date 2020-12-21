b900449fb5a6dbe95be9553eb3e8745c09f7ebad
buckshot307 <buckshot307@gmail.com>
2020-12-21 16:12:44 -0500

    Added promise class
    Code found on medium supposed to fix the problems caused by using FB SDK with react

--- src/hooks/init-fb-sdk.js
+++ src/hooks/init-fb-sdk.js
@@ -2,21 +2,32 @@ import { apiAccount } from './apiAccount'
 
 const FB_APP_ID = process.env.FB_APP_ID
 
-export const InitFacebookSdk = () => {
-  return new Promise((resolve, reject) => {
-    //initialize FB SDK before starting app
-    console.log('Initializing FB SDK')
-    resolve(
-    window.fbAsyncInit = () => {
-      FB.init({
-        appId: FB_APP_ID,
-        cookie: true,
-        xfbml: true,
-        version: 'v9.0',
-        status: true
-      })
+//Creates a Promise to be used for waiting on FB SDK loading before doing other things
+class Deferred {
+  constructor() {
+    var self = this
+    this.promise = new Promise(function (resolve, reject) {
+      self.reject = reject
+      self.resolve = resolve
+    })
+  }
+}
+export const fbLoaded = (new Deferred());
 
-    },
+//Initializes facebook's javascript SDK
+export const InitFacebookSdk = () => {
+  //initialize FB SDK before starting app
+  console.log('Initializing FB SDK')
+  window.fbAsyncInit = () => {
+    FB.init({
+      appId: FB_APP_ID,
+      cookie: true,
+      xfbml: true,
+      version: 'v9.0',
+      status: true
+    })
+    fbLoaded.resolve()
+  },
     //load FB sdk script
     (function (d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
@@ -24,32 +35,30 @@ export const InitFacebookSdk = () => {
       js = d.createElement(s); js.id = id;
       js.src = "https://connect.facebook.net/en_US/sdk.js";
       fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk')))
-    reject(error => {return error})
-  })
+    }(document, 'script', 'facebook-jssdk'))
 }
 
-export const findIfLoggedIn = () => {
-  return new Promise((resolve, reject) => {
-    window.FB.getLoginStatus((response) => {
 
-      /*  response = 
-          {status: ['connected', 'not_authorized', 'unknown'],
-            authResponse : {
-              accessToken: '...',
-              expiresIn: '...',
-              signedRequest: '...',
-              userID: '...'          } }   */
-      console.log(response)
-      if (response.status === 'connected') {
-        resolve(
-          console.log('Connected: ' + response),
-          //if user is logged into FB, and authorized locally
-          apiAccount.FbApiAuth(response.authResponse.userID, response.authResponse.accessToken)
-        )
-      } else {
-        reject(console.log('error not logged in'))
-      }
-    })
+//Checks login state of user.  If logged in, authorizes them with local auth
+export const findIfLoggedIn = () => {
+  window.FB.getLoginStatus((response) => {
+
+    /*  response = 
+        {status: ['connected', 'not_authorized', 'unknown'],
+          authResponse : {
+            accessToken: '...',
+            expiresIn: '...',
+            signedRequest: '...',
+            userID: '...'          } }   */
+    console.log(response)
+    if (response.status === 'connected') {
+      resolve(
+        console.log('Connected: ' + response),
+        //if user is logged into FB, and authorized locally
+        apiAccount.FbApiAuth(response.authResponse.userID, response.authResponse.accessToken)
+      )
+    } else {
+      reject(console.log('error not logged in'))
+    }
   })
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import App from './App'
-import { InitFacebookSdk } from './hooks/init-fb-sdk'
+import { InitFacebookSdk, fbLoaded } from './hooks/init-fb-sdk'
 require('./mystyles.scss')
 import {render} from 'react-dom'
 
 
 const title = 'Aaron\'s Blog'
 
-InitFacebookSdk().then(startApp)
+InitFacebookSdk()
+fbLoaded.promise.then(startApp())
  
 function startApp() {
   render(
