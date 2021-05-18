b9750efd2344c18b71dae33b6f9bf64172f3093c
AaronMikelKey <AaronMikelKey@gmail.com>
2021-05-18 12:11:02 -0400

    updated packages, reformatted Home component

--- src/Home.js
+++ src/Home.js
@@ -1,59 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/navbar';
 import HomeBlogPosts from './components/HOMEblogposts';
 
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
+const Home = (props) => {
+	const [posts, setPosts] = useState({
+		error: null,
+		isLoaded: false,
+		items: []
+	})
 
-  componentDidMount() {
-    fetch("https://aaron-key-blog-api.herokuapp.com/api")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
+	fetch('https://aaron-key-blog-api.herokuapp.com/api')
+		.then(res => res.json())
+		.then(
+			res => setPosts({error: null, isLoaded: true, items: res}),
+			err => setPosts({error: err, isLoaded: true, items: []})
+			)
 
-  render() {
-    const { error, isLoaded, items } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div>
-          <Navbar />
-          <ul>
-            {items.map(item => (
-              <HomeBlogPosts key={item._id} {...item} />
-            ))}
-          </ul>
-        </div>
-      )
-    }
-  }
+	return (
+		posts.error ? <div>Error: {posts.error.message} </div> :
+		!posts.isLoaded ? <div>Loading...</div> :
+		<div>
+			<ul>
+				{posts.items.map(item => (<HomeBlogPosts key={item.id} {...item} />))}
+			</ul>
+		</div>
+	)
 }
 
 export default Home;
--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ const title = 'Aaron\'s Blog'
 
   render(
     <div>
-      <div id="fb-root"></div>
-      <script async defer crossorigin="anonymous"
-        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0&appId=829109204488429&autoLogAppEvents=1"
-        nonce="2dyEbxI3"></script>
       <App title={title} />
     </div>,
     document.getElementById('app')
